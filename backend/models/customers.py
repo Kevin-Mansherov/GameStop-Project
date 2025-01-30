1bc017820857f07d54a12863b67aed87fd567e97
Kevin-Mansherov <kevinmansherov28@gmail.com>
2025-01-30 22:48:40 +0200

    upgrade the html css files

--- backend/models/customers.py
+++ backend/models/customers.py
@@ -4,7 +4,7 @@ from . import db
 class Customers(db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(50),nullable=False)
     phone_number = db.Column(db.String(10), nullable=False)
     city = db.Column(db.String(50), nullable=False)
     age = db.Column(db.Integer, nullable=True)
